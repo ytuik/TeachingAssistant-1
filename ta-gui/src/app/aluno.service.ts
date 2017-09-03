dfe0df8898de34747051c0d581bd2a1f7eaf53f2
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:26:56 -0300

    melhorando o filtro

--- ta-gui/src/app/aluno.service.ts
+++ ta-gui/src/app/aluno.service.ts
@@ -4,11 +4,14 @@ export class AlunoService {
   alunos: Aluno[] = [];
   
   gravar(aluno: Aluno): Aluno {
-    var result : any = null;
-    if (!this.alunos.find(a => a.cpf == aluno.cpf)) {
+    var result: any = null;
+    if (this.cpfNaoCadastrado(aluno.cpf)) {
       this.alunos.push(aluno);
       result = aluno;
     }
     return result;
+  }
+  cpfNaoCadastrado(cpf: string): boolean {
+     return !this.alunos.find(a => a.cpf == cpf);
   }
 }
